cedb31ae686d6c912220fd0fe2d01461cb40c496
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-08-22 17:56:17 -0400

    Add codigos do projeto cadastro de produtos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# produtos
-Projeto para cadastro de produtos usado para exemplos de loadbalancer
+#11-PprojetoWeb-Componentes-Painel
+	Projeto web com Usando Maven, Componentes JSF de Panel Grid e Mensagens.
+	Com css externo e personalização dos campos e botão do formulario usando o css. 
---
+++ src/main/java/com/anselmopfeifer/controller/GestaoProdutosBean.java
@@ -0,0 +1,38 @@
+package com.anselmopfeifer.controller;
+
+/**
+ * Created by anselmo on 22/08/16.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+import com.anselmopfeifer.controller.Produto;
+
+@ManagedBean
+public class GestaoProdutosBean {
+
+    private List<Produto> produtos;
+    private Produto produto;
+
+    public GestaoProdutosBean() {
+        this.produtos = new ArrayList<Produto>();
+        this.produto = new Produto();
+    }
+
+    public void incluir() {
+        this.produtos.add(this.produto);
+        this.produto = new Produto();
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+}
